6e647e0ded250b4cafc53abe2b91b452fb84e983
Serhat Sel <selserhat01@gmail.com>
2024-02-24 16:19:57 +0300

    sadasdasdgfdg

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,19 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import userRouter from "./routes/userRouter.js";
-import conn from "./db.js";
+// import conn from "./db.js";
 
 dotenv.config();
-conn();
+const conn = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI, {});
+    console.log("Connected to MongoDB!");
+  } catch (error) {
+    console.error("MongoDB Error:", error.message);
+    process.exit(1); // Uygulamayı durdur
+  }
+};
+
 const port = process.env.PORT;
 
 const app = express();
@@ -16,6 +25,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/users", userRouter);
+
+conn();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
